66a5de15f6784facaa2156ac8651d02e54e3eb69
Rajesh Morla <Morla.Rajesh@Infinite.com>
2018-08-26 04:50:44 +0530

    Environment variable fixed

--- src/main/java/com/incedo/api/common/AppConstants.java
+++ src/main/java/com/incedo/api/common/AppConstants.java
@@ -2,7 +2,7 @@ package com.incedo.api.common;
 
 public class AppConstants 
 {
-	public static final String AWS_REGION = System.getenv("AWS_REGION");
+	public static final String AWS_REGION = System.getenv("APP_REGION");
 	public static final String SECURITY_TOKEN = System.getenv("X_Incedo_Security_Token");
 	public static final String S3_BUCKET_FOLDER = "ImageStorage/";
 	public static final String S3_BUCKET_NAME = "rajeshmorla.online";
